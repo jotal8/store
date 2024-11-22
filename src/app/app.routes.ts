0bd0115d4212e98f6719f43491c8770c6039c3ac
Julian Otalvaro <jotal8@gmail.com>
2024-11-21 21:25:41 -0500

    Se realiza adecuaciones en la pagina about y 404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,19 @@
 import { Routes } from '@angular/router';
 import { ListComponent } from './domains/products/pages/list/list.component';
 import { AboutComponent } from './domains/info/pages/about/about.component';
+import { NotFoundComponent } from '@info/pages/not-found/not-found.component';
 
 export const routes: Routes = [
+    {
+        path: '',
+        component: ListComponent
+    },
     {
         path: 'about',
         component: AboutComponent
     },
     {
-        path: '',
-        component: ListComponent
-    }
+        path: '**',
+        component: NotFoundComponent
+    },
 ];
--- src/app/domains/info/pages/about/about.component.ts
+++ src/app/domains/info/pages/about/about.component.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { CounterComponent } from '@shared/components/counter/counter.component';
 import { WaveAudioComponent } from '@info/components/wave-audio/wave-audio.component';
 import { HighlightDirective } from '@shared/directives/highlight.directive';
+import { HeaderComponent } from '@shared/components/header/header.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [ CounterComponent, WaveAudioComponent, HighlightDirective],
+  imports: [ CounterComponent, WaveAudioComponent, HighlightDirective, HeaderComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
